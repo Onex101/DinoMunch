e08dc8c57aa59663348863d0cdbd821fc0a0cb8d
markb001 <bradleigh.marks@vcontractor.co.za>
2021-09-12 16:29:40 +0200

    Removed escape key

--- Assets/Scripts/StartScript.cs
+++ Assets/Scripts/StartScript.cs
@@ -12,13 +12,4 @@ public class StartScript : MonoBehaviour
         var player = Instantiate(playerPrefab, new Vector2(-1.58f, -0.578f), Quaternion.identity);
         player.GetComponent<Rigidbody2D>().velocity = new Vector2(2.2f, 0);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetButtonDown("Cancel"))
-        {
-            SceneManager.LoadScene("GameOver");
-        }
-    }
 }

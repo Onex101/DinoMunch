749504aceb8eb13efe609b36d7ce2ded5c6e95fb
Onex101 <xeno@zarobi.co.za>
2021-09-12 10:22:07 +0200

    Dino color change on eat working

--- Assets/Scripts/GenerateShapes.cs
+++ Assets/Scripts/GenerateShapes.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class GenerateShapes : MonoBehaviour
 {
     [SerializeField] private GameObject[] shapes;
-    private Color[] colours = { Color.red, Color.blue, Color.yellow, Color.green };
+
+    private CustomColors customColors = new CustomColors();
+    private Color[] colours = { CustomColors.Red, CustomColors.Blue, CustomColors.Yellow, CustomColors.Green };
     private float gameSpeed = -0.5f;
 
 
@@ -19,20 +20,20 @@ public class GenerateShapes : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void SpawnShape()
     {
-        int shapeType = (int) Random.Range(0, 2.99999f);
-        int colourIndex = (int) Random.Range(0, 3.9999f);
+        int shapeType = (int)Random.Range(0, 2.99999f);
+        int colourIndex = (int)Random.Range(0, 3.9999f);
         float height = Random.Range(-0.6f, 0.4f);
 
         GameObject shapeToSpawn = shapes[shapeType];
         shapeToSpawn.GetComponent<SpriteRenderer>().color = colours[colourIndex];
 
         var shapeSpawned = Instantiate(shapeToSpawn, new Vector2(2, height), Quaternion.identity);
-        
+
     }
 
     private void IncreaseSpeed()
